f96757b5734bf7acf8e43cc51d02dec98f2aaca7
Darren Cook <thelurkingdev@gmail.com>
2018-12-09 13:11:05 -0500

    Set base url and token header for axios requests in main.js.

--- src/main.js
+++ src/main.js
@@ -2,8 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { TokenService } from '../services/storage.service';
+import { ApiService } from '../services/api.service';
 
 Vue.config.productionTip = false
+
+// Set base url of api
+ApiService.init(process.env.VUE_APP_ROOT_API);
+
+// If token exists, set header
+if(TokenService.getToken()) {
+  ApiService.setHeader();
+}
 
 new Vue({
   router,
